db4ed7c19659c7517c21dee0e851c3ba1c0457f2
gary <cmonkey3d@yahoo.com>
2019-01-30 12:29:22 -0500

    have my code in

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,94 +1,168 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
+    <ul v-for="(o,i) in board" v-bind:key="i">
+        <li>{{o}}</li>
     </ul>
   </div>
 </template>
 
 <script>
+// splice
+const TinyQueue=require('tinyqueue')
+const range=(n,spread=a=>a)=>[...Array(n).keys()].map(spread)
+const side=12
+const sRange=range(side)
+const corners=[[-1,-1],[side,side],[-1,side],[side,-1]]
+const outside=sRange.reduce((a,c)=>[...a,[c,-1],[c,side],[-1,c],[side,c]],corners)
+const inside=sRange.reduce((a,c)=>[...a,...sRange.map(d=>[c,d])],[])
+const colors=['a','b','c','d','2','f']
+const rndEl=e=>e[Math.trunc(Math.random()*e.length)]
+const rBoard = ()=>outside.reduce((a,c)=>{a[c]='x';return a},inside.reduce((a,c)=>{a[c]=rndEl(colors);return a;},{}))
+const boardRow=(b,r)=>sRange.map(c=>b.guts[[c,r]]?'.':b.brd[[c,r]]).join('')
+const done=b=>inside.every(a=>b.guts[a])
+const boardStr=b=>sRange.map(r=>boardRow(b,r)).join('\n')
+const dirs=[[1,0],[-1,0],[0,1],[0,-1]]
+const start=[0,0]
+const obAdd=(o,k,v=true)=>{ o[k]=v; return o;}
+const obSub=(o,k)=>{ delete o[k]; return o;}
+
+
+function flood(b,x,y) {
+    let inset={}
+    let outset={}
+    let clr=b.brd[[x,y]]
+    function inFlood(x,y) {
+       let loc=[x,y]
+       if (inset[loc] || b.guts[loc] || outset[loc]) return
+       if (b.brd[loc]!=clr) {
+          outset[loc]=true
+          return
+       } 
+       inset[loc]=true
+       for (d of dirs) inFlood(x+d[0],y+d[1])
+    }
+    inFlood(x,y)
+    return {
+       guts:Object.keys(inset),
+       skin:Object.keys(outset)
+    }
+}
+
+function initFloodState(brd) {
+    let ret = {
+        brd,
+        cons:{},
+        guts:outside.reduce((a,c)=>obAdd(a,c),{}),
+        skin:{}
+    }
+    for (let l of inside) {
+       if (!ret.cons[l]) {
+          let c=flood(ret,l[0],l[1])
+          ret.cons=c.guts.reduce((a,l)=>obAdd(a,l,c),ret.cons)
+       }
+    }
+    ret.skin=ret.cons[start].skin.reduce((a,l)=>obAdd(a,l),ret.skin)
+    ret.guts=ret.cons[start].guts.reduce((a,l)=>obAdd(a,l),ret.guts)
+    return ret
+}
+
+const moves=fs=>Object.keys(Object.keys(fs.skin).reduce((a,c)=>obAdd(a,fs.brd[c]),{}))
+
+function fill(fs,clr) {
+    let nfs={
+        brd:fs.brd,
+        cons:fs.cons,
+        skin:{...fs.skin},
+        guts:{...fs.guts}
+    }
+    for (let loc of Object.keys(fs.skin).filter(l=>fs.brd[l]==clr)) {
+       if (!nfs.guts[loc]) {
+          nfs.guts=nfs.cons[loc].guts.reduce((a,c)=>obAdd(a,c),nfs.guts)
+          nfs.skin=nfs.cons[loc].guts.reduce((a,c)=>obSub(a,c),nfs.skin)
+          nfs.skin=nfs.cons[loc].skin.reduce((a,c)=>nfs.guts[c]?a:obAdd(a,c),nfs.skin)
+       }
+    }
+    return nfs
+}
+
+function distMap(brd) {
+    let map={}
+    let skin=Object.keys(brd.skin)
+    let ct=0;
+    while (skin.length>0) {
+        let nextSkin={}
+        ct+=1
+        for (let loc of skin) {
+           if (!map[loc] && !brd.guts[loc]) {
+               map=brd.cons[loc].guts.reduce((a,c)=>obAdd(a,c,ct),map)
+               nextSkin=brd.cons[loc].skin.reduce((a,c)=>(brd.guts[c] || map[c])?a:obAdd(a,c),nextSkin)
+           }
+        }
+        skin=Object.keys(nextSkin)
+    }
+    return map
+}
+
+const colorDist=(b,dm=distMap(b))=>colors.map(c=>inside.filter(i=>!b.guts[i] && b.brd[i]==c).reduce((a,c)=>dm[c]>a?dm[c]:a,0))
+
+function solve(fs) {
+    let q=new TinyQueue([{fs,moves:[],est:20,cm:colorDist(fs)}],(a,b)=>(a.moves.length+a.est)-(b.moves.length+b.est))
+    let startDist=colorDist(fs,distMap(fs)).reduce((a,c)=>a+=c,0)
+    let visited={}
+    let best=[]
+    const meval=300
+    let evals=meval
+    while (q.length>0) {
+       let b=q.pop()
+       if ((best.length==0) || ((b.moves.length+Math.max(...b.cm,b.cm.filter(a=>a>0).length))<(best.length))) {
+           for (let m of moves(b.fs)) {
+              let fs=fill(b.fs,m)
+              if (done(fs)) {
+                 best= [...b.moves,m]
+                 console.log('best ',best.length,evals)
+                 evals=300
+                 continue
+              }
+              let cs=Object.keys(fs.guts).join(',')
+              let cm=colorDist(fs,distMap(fs))
+              if (!visited[cs]) {
+                 evals-=1
+                 if ((best.length>0)&&(evals<0))
+                    return best
+                 visited[cs]=true
+                 q.push( {
+                    fs,
+                    // add a bonus for closing out a color
+                    est:((20*cm.reduce((a,c)=>a+=c,0))/startDist)-(cm.filter(c=>c==0).length*3),
+                    moves:[...b.moves,m],
+                    cm
+                 } )
+              }
+           } 
+         }
+    }
+    console.log('evaluated ',Object.keys(visited).length,best.length)
+    return best
+}
+
+const disp2=(h,c)=> sRange.map(r=>boardRow(h,r)+'|-|-|'+boardRow(c,r)).join('\n')
+
+function clear(b) {
+   let m=distMap(b)
+   return colors.filter(c=>Object.keys(m).filter(l=>b.brd[l]==c)
+                    .reduce((a,l)=>a>m[l]?a:m[l],0)==1)
+}
+
+
+// end splice
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'flood',
+      board: Object.keys(rBoard())
     }
   }
 }
